fb4175471b0bb381fa38639ffa496196d77c0a46
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-06-27 15:13:48 +0200

    title + task input

--- index.php
+++ index.php
@@ -4,11 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>todo-list</title>
 </head>
 
 <body>
-
+    <header>
+        <h1 class="main-ttl">Todo-list</h1>
+    </header>
+    <main class="container">
+        <div>
+            <input type="text">
+            <input type="button">
+            <div>
+                <p>deadline :</p>
+                <input type="date">
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
