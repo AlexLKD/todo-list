067f374ee7110c5eb4c68b9b8a8aa9438a09b003
Boismain Florian <boismain.florian@gmail.com>
2023-06-27 16:51:07 +0200

    css and bdd

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+try {
+    $dbCo = new PDO(
+    'mysql:host=localhost;dbname=todo_list;charset=utf8',
+'phplocal',
+'phplocal'
+);
+$dbCo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
+PDO::FETCH_ASSOC);
+}
+catch (Exception $e) {
+die('Unable to connect to the database.
+'.$e->getMessage());
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,6 +43,17 @@
 
         <section>
             <?php
+                
+            $query = $dbCo->prepare("SELECT text FROM task;");
+            $query->execute();
+            $result = $query->fetchAll();
+
+
+            // foreach($result as $product) {
+            //     echo '<li>'.$product['text'].'</li>';
+            //     }
+     
+                
             $tasks = [
                 ['id' => '1', 'text' => 'task 1', 'date' => '2022-10', 'status' => 1],
                 ['id' => '2', 'text' => 'task 2', 'date' => '2022-15', 'status' => 1],
@@ -32,17 +62,20 @@
                 ['id' => '5', 'text' => 'task 5', 'date' => '2022-11', 'status' => 1],
                 ['id' => '6', 'text' => 'task 6', 'date' => '2022-09', 'status' => 1]
             ];
+
+
             function createMenu(array $tasks): string
             {
                 $menu = '<ul class="main-nav-list">';
                 foreach ($tasks as $task) {
-                    $menu .= '<li class=""> ' . $task['text'] . '</li>';
+                    $menu .= '<li class="main-nav-item"> ' . $task['text'] . '</li>' ;
                 }
                 $menu .= '</ul>';
                 return $menu;
             }
+
             ?>
-            <?= createMenu($tasks) ?>
+            <?= createMenu($result) ?>
         </section>
     </main>
 </body>
