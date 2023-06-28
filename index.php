8bde6a955f169d96f9fe5d59d6baba56278241f4
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-06-28 12:18:14 +0200

    add required *

--- index.php
+++ index.php
@@ -1,3 +1,52 @@
+<?php
+try {
+    $dbCo = new PDO(
+        'mysql:host=localhost;dbname=todo-list;charset=utf8',
+        'phplocal',
+        'phplocal'
+    );
+    $dbCo->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC
+    );
+} catch (Exception $e) {
+    die('Unable to connect to the database.
+        ' . $e->getMessage());
+}
+
+if (isset($_POST['submit'])) {
+    $task = $_POST['task'];
+    $dateCreate = date('Y-m-d H:i:s');
+    $query = $dbCo->prepare("INSERT INTO task (text, date_create) VALUES (:text, :date_create)");
+    $query->execute([
+        ':text' => strip_tags($task),
+        ':date_create' => $dateCreate
+    ]);
+}
+
+if (isset($_GET['delete'])) {
+    $taskId = $_GET['delete'];
+    $query = $dbCo->prepare("DELETE FROM task WHERE Id_task = :taskId");
+    $query->execute([
+        ':taskId' => strip_tags($taskId)
+    ]);
+}
+
+if (isset($_GET['validate'])) {
+    $taskId = $_GET['validate'];
+    $query = $dbCo->prepare("UPDATE task SET status = '2' WHERE Id_task = :taskId");
+    $query->execute([
+        ':taskId' => strip_tags($taskId)
+    ]);
+}
+if (isset($_GET['invalidate'])) {
+    $taskId = $_GET['invalidate'];
+    $query = $dbCo->prepare("UPDATE task SET status = '1' WHERE Id_task = :taskId");
+    $query->execute([
+        ':taskId' => strip_tags($taskId)
+    ]);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,69 +63,30 @@
     </header>
     <main class="container">
         <form action="" method="POST">
-            <input type="text" name="task" placeholder="task" class="form-txt">
-            <input type="submit" class="form-cta" name="submit" value="✔️">
-            <div>
-                <p>à faire avant le :</p>
-                <input type="date" name="due_date">
+            <input type="text" name="task" placeholder="task" class="form-txt" required>
+            <span class="required">*</span>
+            <div class="form-date">
+                <div>
+                    <p>à faire avant le :</p>
+                    <input type="date" name="due_date">
+                </div>
+                <input type="submit" class="form-cta" name="submit" value="✔️">
             </div>
         </form>
 
         <section>
             <?php
-            try {
-                $dbCo = new PDO(
-                    'mysql:host=localhost;dbname=todo-list;charset=utf8',
-                    'phplocal',
-                    'phplocal'
-                );
-                $dbCo->setAttribute(
-                    PDO::ATTR_DEFAULT_FETCH_MODE,
-                    PDO::FETCH_ASSOC
-                );
-            } catch (Exception $e) {
-                die('Unable to connect to the database.
-        ' . $e->getMessage());
-            }
-
-
-            if (isset($_POST['submit'])) {
-                $task = $_POST['task'];
-                $dateCreate = date('Y-m-d H:i:s');
-                $query = $dbCo->prepare("INSERT INTO task (text, date_create) VALUES (:text, :date_create)");
-                $query->execute([
-                    ':text' => strip_tags($task),
-                    ':date_create' => $dateCreate
-                ]);
-            }
-
-            if (isset($_GET['delete'])) {
-                $taskId = $_GET['delete'];
-                $query = $dbCo->prepare("DELETE FROM task WHERE Id_task = :taskId");
-                $query->execute([
-                    ':taskId' => strip_tags($taskId)
-                ]);
-            }
-
-            if (isset($_GET['validate'])) {
-                $taskId = $_GET['validate'];
-                $query = $dbCo->prepare("UPDATE task SET status = '2' WHERE Id_task = :taskId");
-                $query->execute([
-                    ':taskId' => strip_tags($taskId)
-                ]);
-            }
-
-
             $query = $dbCo->prepare("SELECT Id_task, text FROM task WHERE status = '1' ORDER BY date_create ASC");
             $query->execute();
             $result = $query->fetchAll();
             echo '<h2>à faire</h2><ul class="main-nav-list">';
             foreach ($result as $task) {
                 echo '<li class="main-nav-item">' . $task['text'] . ' 
+                <div draggable="true">
                     <a href="index.php?validate=' . $task['Id_task'] . '" class="validate-link"><button type="submit" 
                     class="validate-button button" name="validate" value="' . $task['Id_task'] . '">✔️</button></a>
                     <a href="index.php?delete=' . $task['Id_task'] . '" class="delete-link"><button type="submit" 
-                    class="delete-button button" name="delete" value="' . $task['Id_task'] . '">❌</button></a>
+                    class="delete-button button" name="delete" value="' . $task['Id_task'] . '">❌</button></a><div>
                 </li>';
             }
             echo '</ul>';
@@ -89,11 +99,12 @@
             $query = $dbCo->prepare("SELECT Id_task, text FROM task WHERE status = '2' ORDER BY date_create ASC");
             $query->execute();
             $result = $query->fetchAll();
-            echo '<h2>à faire</h2><ul class="main-nav-list">';
+            echo '<h2>fait</h2><ul class="main-nav-list">';
             foreach ($result as $task) {
                 echo '<li class="main-nav-item">' . $task['text'] . ' 
-                <div class="main-nav-form" action="" method="POST" class="delete-form">
-                    <a href="index.php?validate=' . $task['Id_task'] . '" class="validate-link"></a>
+                <div>
+                    <a href="index.php?invalidate=' . $task['Id_task'] . '" class="invalidate-link"><button type="submit" 
+                    class="invalidate-button button" name="invalidate" value="' . $task['Id_task'] . '">❎</button></a>
                     <a href="index.php?delete=' . $task['Id_task'] . '" class="delete-link"><button type="submit" 
                     class="delete-button button" name="delete" value="' . $task['Id_task'] . '">❌</button></a></div> 
                     </li>';
@@ -102,6 +113,7 @@
             ?>
         </section>
     </main>
+    <script src="script.js"></script>
 </body>
 
 </html>
