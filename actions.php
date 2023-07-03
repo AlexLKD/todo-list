68a62971548668b171c20f3a5dd2d773610f09e0
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-07-03 12:31:13 +0200

    reminder date done on submit and edit

--- actions.php
+++ actions.php
@@ -14,11 +14,13 @@ if (isset($_POST['submit'])) {
 
     $task = $_POST['task'];
     $dateCreate = date('Y-m-d H:i:s');
-    $query = $dbCo->prepare("INSERT INTO task (text, date_create, ranking) VALUES (:text, :date_create, :ranking)");
+    $newDate = $_POST['new_date'];
+    $query = $dbCo->prepare("INSERT INTO task (text, date_create, ranking, recall) VALUES (:text, :date_create, :ranking, :newDate)");
     $isOk = $query->execute([
         ':text' => strip_tags($task),
         ':date_create' => $dateCreate,
-        ':ranking' => intval($result['newPriority'])
+        ':ranking' => intval($result['newPriority']),
+        ':newDate' => $newDate
     ]);
     header('Location: index.php?msg=' . ($isOk ? 'La tâche a été ajoutée' : 'Un problème a été rencontré lors de l\'ajout de la tâche'));
     exit;
@@ -41,7 +43,7 @@ if (isset($_GET['delete'])) {
     if ($query->rowCount()) {
         // echo '<p class="transition" id="message"> La tâche a été supprimée.</p>';
     };
-    header('Location: tasks-done.php?msg=' . ($isOk ? 'La tâche a été supprimée' : 'La tâche n\'a pas pu être supprimée'));
+    header('Location: index.php?msg=' . ($isOk ? 'La tâche a été supprimée' : 'La tâche n\'a pas pu être supprimée'));
     exit;
 }
 
@@ -85,9 +87,11 @@ if (isset($_GET['invalidate'])) {
 if (isset($_POST['update'])) {
     $taskId = $_POST['update'];
     $newTask = $_POST['new_task'];
-    $query = $dbCo->prepare("UPDATE task SET text = :newTask WHERE Id_task = :taskId");
+    $newDate = $_POST['new_date'];
+    $query = $dbCo->prepare("UPDATE task SET text = :newTask, recall = :newDate WHERE Id_task = :taskId");
     $isOk = $query->execute([
         ':newTask' => strip_tags($newTask),
+        ':newDate' => strip_tags($newDate),
         ':taskId' => strip_tags($taskId)
     ]);
     if ($query->rowCount()) {
@@ -95,34 +99,7 @@ if (isset($_POST['update'])) {
     header("Location: index.php?msg=" . ($isOk ? 'Tâche mise à jour' : 'Impossible de mettre à jour'));
     exit;
 }
-// if (isset($_POST['update'])) {
-//     $taskId = $_POST['update'];
-//     $newTask = $_POST['new_task'];
-//     $dateReminder = isset($_POST['date_reminder']);
 
-
-//     $query = $dbCo->prepare("UPDATE task SET text = :newTask, date_reminder = :dateReminder WHERE Id_task = :taskId");
-
-//     // Check if there's a reminder date already
-//     if ($dateReminder !== null) {
-//         $query->bindValue(':dateReminder', $dateReminder);
-//     } else {
-//         // If there's no reminder set then ignore 
-//         $query->bindValue(':dateReminder', null, PDO::PARAM_NULL);
-//     }
-
-//     $isOk = $query->execute([
-//         ':newTask' => strip_tags($newTask),
-//         ':taskId' => strip_tags($taskId)
-//     ]);
-
-//     if ($query->rowCount()) {
-//         // La tâche a été mise à jour avec succès
-//     }
-
-//     header("Location: index.php?msg=" . ($isOk ? 'Tâche mise à jour' : 'Impossible de mettre à jour'));
-//     exit;
-// }
 
 //-----------------------------------------------------------
 
@@ -157,42 +134,3 @@ if (array_key_exists('rank', $_GET) && $_GET['prior'] == 'down') {
     header("Location: index.php");
     exit;
 }
-
-
-// // UPDATE TASK
-// if (isset($_POST['update'])) {
-//     $taskId = $_POST['update'];
-//     $newTask = $_POST['new_task'];
-//     $query = $dbCo->prepare("UPDATE task SET text = :newTask WHERE Id_task = :taskId");
-//     $isOk = $query->execute([
-//         ':newTask' => strip_tags($newTask),
-//         ':taskId' => strip_tags($taskId)
-//     ]);
-//     if ($query->rowCount()) {
-//     }
-//     header("Location: index.php?msg=" . ($isOk ? 'Tâche mise à jour' : 'Impossible de mettre à jour'));
-//     exit;
-// }
-
-
-if(isset($_POST['call'])){
-    $taskId = $_POST['call'];
-    $newDate = $_POST['new_date'];
-    $query = $dbCo->prepare("UPDATE task SET recall = :newDate WHERE Id_task = :taskId");
-    $isOk = $query->execute([
-        ':newDate' => strip_tags($newDate),
-        ':taskId' => strip_tags($taskId)
-    ]);
-    if ($query->rowCount()) {
-    }
-    header("Location: index.php?msg=" . ($isOk ? 'Tâche mise à jour' : 'Impossible de mettre à jour'));
-    exit;
-}
-
-function getRecallFromToday($task ){
-    $dt = date('Y-m-d');
-    if($task['recall'] === $dt){
-        echo ' <p>' .$task['text'].' </p> ' ; }}
-
-
-
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 require 'includes/_database.php';
 require 'includes/_functions.php';
+
+session_start();
+$_SESSION['token'] = md5(uniqid(mt_rand(), true));
+// var_dump($_SESSION['token']);
+// exit;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,7 +28,7 @@ require 'includes/_functions.php';
             <div class="form-date">
                 <div>
                     <p>Rappel le :</p>
-                    <input type="date" name="due_date">
+                    <input type="date" id="recall" name="new_date" value="">
                 </div>
                 <input type="submit" class="form-cta" name="submit" value="✔️">
             </div>
@@ -33,35 +38,35 @@ require 'includes/_functions.php';
             <a href="tasks-done.php"><button type="button">Tasks done</button></a>
             <?php
             // <input type="date" name="date_reminder" class="date-input" value="' . $task['date_reminder'] . '">
-            
+
             // function getRecallFromToday($task ){
             //     $dt = date('Y-m-d');
             //     if($task['recall'] === $dt){
             //         echo ' <p>' .$task['text'].' </p> ' ; }}
 
-                    $query = $dbCo->prepare("SELECT theme ,recall, Id_task, text, ranking FROM task WHERE status = '1' ORDER BY ranking DESC");
-                    $query->execute();
-                    $result = $query->fetchAll();
-                //   echo  selectTheme($result);
-              
-                   
-                    
-                    echo '<h2>à faire</h2><ul class="main-nav-list"> ';
-                    
-                    echo '<label for="pet-select">Choose theme:</label>
+            $query = $dbCo->prepare("SELECT theme ,recall, Id_task, text, ranking FROM task WHERE status = '1' ORDER BY ranking DESC");
+            $query->execute();
+            $result = $query->fetchAll();
+            //   echo  selectTheme($result);
+
+
+
+            echo '<h2>à faire</h2><ul class="main-nav-list"> ';
+
+            echo '<label for="pet-select">Choose theme:</label>
                     <select name="theme" id="theme-task"> 
                     <option value="">--Please choose an option--</option> 
                     ';
-                    
-                    echo implode("" ,selectTheme($result));
-                    // il faut que pour chaque valeur, option value doit ajouté
-                    echo '</select>';
-                    
-                     
-                    if (array_key_exists('msg', $_GET)) {
-                        echo '<p class="task-info">' . $_GET['msg'] . '</p>';
-                    }
-                    foreach ($result as $task) {
+
+            echo implode("", selectTheme($result));
+            // il faut que pour chaque valeur, option value doit ajouté
+            echo '</select>';
+
+
+            if (array_key_exists('msg', $_GET)) {
+                echo '<p class="task-info">' . $_GET['msg'] . '</p>';
+            }
+            foreach ($result as $task) {
                 // var_dump(date('Y-m-d'));
                 // exit;
                 echo '<li class="main-nav-item">
@@ -70,16 +75,10 @@ require 'includes/_functions.php';
                             <form action="actions.php" method="POST" class="update-form">
                                 <input type="hidden" name="update" value="' . $task['Id_task'] . '">
                                 <input type="text" name="new_task" class="task-input hidden" value="' . $task['text'] . '" required>
+                                <input type="date" id="recall_' . $task['Id_task'] . '" name="new_date" class="" value="' . $task['recall'] . '">
                                 <button type="button" class="edit-button button" data-task-id="' . $task['Id_task'] . '">Edit</button>
                                 <button type="submit" class="update-button button hidden">Save</button>
                             </form>
-                        </div>
-                        <div>
-                        <form action="actions.php" method="POST" class="update-form">
-                        <input type="hidden" name="call" value="' . $task['Id_task'] . '">
-                        <input type="date" id="recall" name="new_date" required="" value= "'. $task['recall'] .'" >  
-                        <button type="submit" class="update-button button">Call</button>
-                        </form>
                         </div>
                         <div>
                             <a href="actions.php?validate=' . $task['Id_task'] . '" class="validate-link"><button type="submit" 
@@ -90,7 +89,6 @@ require 'includes/_functions.php';
                         <div><a href="actions.php?id=' . $task['Id_task'] . '&rank=' . $task['ranking'] . '&prior=down"><img class="arrow" src="img/down.png" alt="down"></a>
                         <a href="actions.php?id=' . $task['Id_task'] . '&rank=' . $task['ranking'] . '&prior=up"><img class="arrow" src="img/up.png" alt="up"></a></div>
                     </li>';
-                    
             }
             echo '</ul>';
             getRecallToday($result)
